5d73782c09c806a760e537eb6a8c867d202b0c07
MuhammadBilalEhsan <bilalprogrammer09@gmail.com>
2021-10-31 06:31:08 +0500

    some ui changes occured

--- frontend/src/Pages/Attendance.js
+++ frontend/src/Pages/Attendance.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserSidebar from "./UserSidebar.js";
+import Checkbox from "@mui/material/Checkbox";
 
 const Attendance = () => {
+  const [todayAttend, setTodayAttend] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setTodayAttend(true);
+    console.log(todayAttend);
+  };
 
   return (
     <>
@@ -10,11 +18,28 @@ const Attendance = () => {
 
         <div className="dashboard">
           <div className="sub_dash sub_dash_att">
-            
+            <div className="attendance_top">
+              <h1>Check Today's Attendance</h1>
+
+              {todayAttend ? (
+                <Checkbox size="large" disabled checked />
+              ) : (
+                <Checkbox
+                  size="large"
+                  color="success"
+                  onChange={(e) => handleChange(e)}
+                />
+              )}
+            </div>
+
+            <div className="attendance_bot">
+              <div className="attendance_bot_left"></div>
+              <div className="attendance_bot_right"></div>
+            </div>
           </div>
         </div>
       </div>
     </>
   );
 };
-export default Attendance
+export default Attendance;
